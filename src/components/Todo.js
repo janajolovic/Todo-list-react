84bbcbcd4b6661a09239742f0edf8c8be287e88a
Jana Jolovic <janajolovic04@gmail.com>
2021-12-23 13:06:49 +0100

    completeHanfler

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,10 +4,13 @@ const Todo = ({text, todo, setTodos, todos}) => {
     const deleteHandler = () => {
         setTodos(todos.filter(el => el.id !== todo.id));
     }
+    const completeHandler = () => {
+        
+    }
     return (
         <div className="todo">
             <li className="todo-item">{text}</li>
-            <button className="complete-btn"><i className="fas fa-check"></i></button>
+            <button onClikc={completeHandler} className="complete-btn"><i className="fas fa-check"></i></button>
             <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
     )
