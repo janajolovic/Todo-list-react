3428208da5bf5b78b413e68e07123b2c20225060
Jana Jolovic <janajolovic04@gmail.com>
2021-12-23 13:14:54 +0100

    completeHandler

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,11 +5,18 @@ const Todo = ({text, todo, setTodos, todos}) => {
         setTodos(todos.filter(el => el.id !== todo.id));
     }
     const completeHandler = () => {
-        
+        setTodos(todos.map(item => {
+            if (item.id === todo.id) {
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+            return item
+        }))
     }
     return (
         <div className="todo">
-            <li className="todo-item">{text}</li>
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
             <button onClikc={completeHandler} className="complete-btn"><i className="fas fa-check"></i></button>
             <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
